e64ea863968a9175d86683ae4b1c282ce60dc8f7
tspeed90 <tspeed90@gmail.com>
2018-06-03 13:08:39 +0100

    refactors js file to remove global variables by using object to store weather values

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ let cityDisplay = document.querySelector('.city');
 let tempDisplay = document.querySelector('.temp');
 let iconDisplay = document.querySelector('.icon-display');
 let messageDisplay = document.querySelector('temp-display');
+let convertTemp = document.querySelector('.convert-temp');
 let currentTemp;
-let degreesC;
-let city;
-let weather;
-let description;
+
 
 requestLocation();
 function requestLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
-    getWeather(lat, long);
-  }, displayError);
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      getWeather(lat, long);
+    }, displayError);
   } else {
     displayError();
   }
@@ -24,41 +22,38 @@ function requestLocation() {
 function displayError() {
   document.querySelector("h1").innerText = "Sorry, unable to get your location."
 }
-const getWeatherButton = document.querySelector('.get-weather');
-const convertTemp = document.querySelector('.convert-temp');
-convertTemp.addEventListener('click', convertDegrees);
+
 
 function getWeather(lat, long) {
   let url = 'https://fcc-weather-api.glitch.me/api/current?lon=' + long + '&lat=' + lat;
-  fetch(url).then(function(response) {
+  fetch(url).then(function (response) {
     return response.json();
-  }).then(function(data) {
-    degreesC = data.main.temp;
-    city = data.name;
-    weather = data.weather[0].main;
-    description = data.weather[0].description;
-    currentTemp = degreesC;
+  }).then(function (data) {
+    let weather = {
+      degreesC: data.main.temp,
+      city: data.name,
+      shortDesc: data.weather[0].main,
+      longerDesc: data.weather[0].description,
+    }
+    currentTemp = weather.degreesC;
+    tempDisplay.setAttribute('data-f', ((9 / 5 * weather.degreesC) + 32).toFixed(1));
+    tempDisplay.setAttribute('data-c', weather.degreesC);
     showTemp(weather);
   });
 }
+convertTemp.addEventListener('click', toggleScale);
 
-function convertDegrees() {
-  let degreesF = (9/5 * degreesC) + 32;
-  currentTemp = currentTemp === degreesC ? degreesF : degreesC;
-  showTemp(weather);
-}
 function showTemp(weather) {
+  tempDisplay.textContent = currentTemp + ' \u00B0C';
   convertTemp.style.display = "block";
-  convertTemp.textContent = currentTemp === degreesC ? " Get \u00B0F" : " Get \u00B0C";
+  convertTemp.textContent = "Get \u00B0F";
   document.querySelector('.temp-display').style.display = "block";
-  cityDisplay.textContent = city + ":";
-  let scale = currentTemp === degreesC ? "C" : "F";
-  tempDisplay.textContent = currentTemp.toFixed(1) + "\u00B0" + scale;
-  document.querySelector('.description').textContent = description;
+  cityDisplay.textContent = weather.city;
+  document.querySelector('.description').textContent = weather.description;
 
   //add font awesome icon to match weather.main info from API
   //weather.main info is from https://openweathermap.org/weather-conditions
-  switch(weather) {
+  switch (weather) {
     case "Clear":
       iconDisplay.classList.add("fa-sun-o");
       break;
@@ -79,3 +74,19 @@ function showTemp(weather) {
       break;
   }
 }
+
+
+function toggleScale() {
+  // cast value of dataset to a number with +
+  if (currentTemp === +tempDisplay.dataset.c) {
+    currentTemp = +tempDisplay.dataset.f;
+    tempDisplay.textContent = tempDisplay.dataset.f + ' \u00B0F';
+    convertTemp.textContent = 'Get \u00B0C';
+  } else {
+    currentTemp = +tempDisplay.dataset.c;
+    tempDisplay.textContent = tempDisplay.dataset.c + ' \u00B0C';
+    convertTemp.textContent = 'Get \u00B0F';
+
+  }
+}
+
