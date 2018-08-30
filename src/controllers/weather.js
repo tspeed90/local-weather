d209b29c7c70c849c5912ae9b5f5bba7fb71f684
tspeed90 <tspeed90@gmail.com>
2018-08-30 14:51:37 +0100

    use weatherData to identify correct icon and render icon on weather page

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -1,8 +1,16 @@
 exports.get = (req, res) => {
-  const { getWeather } = require('../models/weatherForecast.js');
+  const { getWeather, getIcon } = require('../models/weatherForecast.js');
   const { lat, lon, city } = req.query;
 
-  getWeather(lat, lon, city).then(weatherData => {
-    res.render('weather', { weatherData });
-  });
+  getWeather(lat, lon, city)
+    .then(weatherData => {
+      return (forecast = {
+        weatherData,
+        icon: getIcon(weatherData.weather[0].description)
+      });
+    })
+    .then(forecast => {
+      console.log(forecast);
+      res.render('weather', { forecast });
+    });
 };
--- src/models/weatherForecast.js
+++ src/models/weatherForecast.js
@@ -24,4 +24,21 @@ const getWeather = (lat, lon, city) => {
   }
 };
 
-module.exports = { getWeather };
+const getIcon = weatherDescriptions => {
+  const icons = {
+    'clear sky': '/assets/svg/wi-day-sunny.svg',
+    'few clouds': '/assets/svg/wi-day-cloudy.svg',
+    'scattered clouds': '/assets/svg/wi-cloud.svg',
+    'overcast clouds': '/assets/svg/wi-day-cloudy-high.svg',
+    'broken clouds': '/assets/svg/wi-cloudy.svg',
+    'shower rain': '/assets/svg/wi-day-showers.svg',
+    rain: '/assets/svg/wi-day-rain.svg',
+    thunderstorm: '/assets/svg/wi-day-storm-showers.svg',
+    snow: '/assets/svg/wi-day-snow.svg',
+    mist: '/assets/svg/wi-day-sprinkle.svg'
+  };
+
+  return icons[weatherDescriptions];
+};
+
+module.exports = { getWeather, getIcon };
