cfb3f49e621dd0c4a77f4607fb58507566113f16
tspeed90 <tspeed90@gmail.com>
2018-08-30 15:04:36 +0100

    truncate temperature

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -2,12 +2,16 @@ exports.get = (req, res) => {
   const { getWeather, getIcon } = require('../models/weatherForecast.js');
   const { lat, lon, city } = req.query;
 
+  const roundTemperature = temperature => {
+    return Math.trunc(temperature);
+  };
+
   getWeather(lat, lon, city)
     .then(weatherData => {
       return (forecast = {
         city: weatherData.name,
         country: weatherData.sys.country,
-        temperature: weatherData.main.temp,
+        temperature: roundTemperature(weatherData.main.temp),
         description: weatherData.weather[0].description,
         icon: getIcon(weatherData.weather[0].description)
       });
