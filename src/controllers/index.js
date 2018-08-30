151cdc2a2c9266b405ac7c787f01b844387ba46e
tspeed90 <tspeed90@gmail.com>
2018-08-30 22:22:21 +0100

    clean up unused controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const router = express.Router();
 
 const search = require('./search');
 const weather = require('./weather');
-const error = require('./error');
-
-router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/', search.get);
 router.get('/weather', weather.get);
-
-router.use(error.client);
-router.use(error.server);
 
 module.exports = router;
